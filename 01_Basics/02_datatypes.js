504565d614b58e0697014ade2b286fc104d046b1
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-04-22 13:46:35 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log("Anushka")
+
+let name = "Riya" // string
+let age = 19
+let isLoggedIn = false
+let temperature = null;
+let state; // undefined
+
+
+// DATATYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof age);
+console.log(typeof null); // type is object
+console.log(typeof null); // type is undefined
+
+
